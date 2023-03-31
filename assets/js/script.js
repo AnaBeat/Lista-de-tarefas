b5391aa65f0a42bbadd5c94eefe4a5d78a8afc47
AnaBeat <abeatrizbcosta@gmail.com>
2023-03-30 22:10:49 -0300

    Feature - Adiciona função para filtrar tarefas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,46 @@
 const listaInput = document.querySelector(".lista__input input"),
+filtros = document.querySelectorAll(".controles__filtros span"),
 tarefasBox = document.querySelector(".tarefas");
 
 let editaId;
 let tarefaEditada = false;
-
 let tarefas = JSON.parse(localStorage.getItem("lista-tarefas"));    
 
-function mostraTarefa() {
+filtros.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        mostraTarefa(btn.id);
+
+    });
+});
+
+function mostraTarefa(filtro) {
     let li = "";
     if (tarefas) {
         tarefas.forEach((tarefa, id) => {
-            let tarefaCompleta = tarefa.status == "verificada" ? "completa" : "";
-            li += `<li class="tarefa">
-                <label for="${id}">
-                    <input onclick="atualizaStatus(this)" type="checkbox" id="${id}" ${tarefaCompleta}>
-                    <p class="${tarefaCompleta}">${tarefa.name}</p>
-                </label>
-                <div class="tarefa__configuracoes">
-                    <i onclick="mostraMenu(this)" class="fa-solid fa-ellipsis"></i>
-                    <ul class="tarefa__menu">
-                        <li onclick = "editaTarefa(${id}, '${tarefa.name}')" ><i class="fa-solid fa-pencil"></i>Editar</li>
-                        <li onclick = "deletaTarefa(${id})" ><i class="fa-solid fa-trash-can"></i>Excluir</li>
-                    </ul>
-                </div>
-            </li>`
+            let tarefaCompleta = tarefa.status == "completa" ? "verificada" : "";
+            if(filtro == tarefa.status || filtro == "todos"){
+                li += `<li class="tarefa">
+                    <label for="${id}">
+                        <input onclick="atualizaStatus(this)" type="checkbox" id="${id}" ${tarefaCompleta}>
+                        <p class="${tarefaCompleta}">${tarefa.name}</p>
+                    </label>
+                    <div class="tarefa__configuracoes">
+                        <i onclick="mostraMenu(this)" class="fa-solid fa-ellipsis"></i>
+                        <ul class="tarefa__menu">
+                            <li onclick = 'editaTarefa(${id}, "${tarefa.name}")' ><i class="fa-solid fa-pencil"></i>Editar</li>
+                            <li onclick = 'deletaTarefa(${id}, "${filtro}")' ><i class="fa-solid fa-trash-can"></i>Excluir</li>
+                        </ul>
+                    </div>
+                </li>`;
+            }
         });
     }
-    tarefasBox.innerHTML = li;
+    tarefasBox.innerHTML = li || '<span>Você não possui nenhuma tarefa aqui</span>';
 }
 
-mostraTarefa();
+mostraTarefa("todos");
 
 function mostraMenu(tarefaSelecionada) {
     let tarefaMenu = tarefaSelecionada.parentElement.lastElementChild;
@@ -46,13 +57,16 @@ function editaTarefa(id, tarefa){
     editaId = id;
     tarefaEditada = true;
     listaInput.value = tarefa;
+    listaInput.focus();
+    listaInput.classList.add("active");
 }
 
-function deletaTarefa(deleteId) {
+function deletaTarefa(deleteId, filtro) {
+    tarefaEditada = false;
     tarefas.splice(deleteId, 1);
     localStorage.setItem("lista-tarefas", JSON.stringify(tarefas));
     
-    mostraTarefa();
+    mostraTarefa(filtro);
 }
 
 function atualizaStatus(tarefaSelecionada) {
@@ -72,18 +86,17 @@ listaInput.addEventListener("keyup", e => {
     let tarefaUsuario = listaInput.value.trim();
     if (e.key == "Enter" && tarefaUsuario) {
         if (!tarefaEditada) {
-            if (!tarefas) {
-                tarefas = [];
+                tarefas = !tarefas ? [] : tarefas;
+                let infoTarefa = {name: tarefaUsuario, status: "pendente"};
+                tarefas.push(infoTarefa);
             }
-            let infoTarefa = {name: tarefaUsuario, status: "pendente"};
-            tarefas.push(infoTarefa);
-        } else {
-            tarefaEditada = true;
+        else {
+            tarefaEditada = false;
             tarefas[editaId].name = tarefaUsuario;
         }
         
         listaInput.value = "";
         localStorage.setItem("lista-tarefas", JSON.stringify(tarefas));
-        mostraTarefa();
+        mostraTarefa(document.querySelector("span.active").id);
     }
-})
+});
